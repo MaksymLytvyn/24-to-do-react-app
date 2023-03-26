224a69a905a881378ba0c7eb9ea8a3ddcf2669ae
MaksymLytvyn <maxim.lytvyn@gmail.com>
2023-03-26 16:15:52 +0200

    26

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {useState} from "react";
+
+import './App.css';
+import TodoForm from "./components/Todos/TodoForm";
+import TodoList from "./components/Todos/TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+        <TodoForm/>
+        <TodoList todos={todos}/>
+    </div>
+  );
+}
+
+export default App;
